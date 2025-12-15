fac6becfb77911c9bdf7b183d785ea1fd866901b
TheVikasPawar <vikaspawar.connect@gmail.com>
2025-12-15 08:38:59 +0530

    day1morning

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+//Playlist URL - https://www.youtube.com/playlist?list=PLqleLpAMfxGCeYF_4IA3_exiB6cC-FQ_L
+//-----------------------------------------------------------------------------------------------------
 
-using System;
+//Video - 1 - Introduction
 
-namespace Program
-{
-    class First
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Vikas is here...");
-        }
-    }
-}
+//installation
+//dotnet latest version download and install
+//vs code 
+
+//create new simple project
+//cmd - 1.dotnet new console -n Project_Name
+//      2.dotnet run
+
+// using System;
+
+// namespace Program
+// {
+//     class First
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Vikas is here...");
+//         }
+//     }
+// }
+
+
+//Video 2 - DataTypes and Variables
+
